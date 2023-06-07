9c5aae0f1fcf8afc207bf1b8afce0887eedbe5f7
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-07 12:17:10 +0300

    Классы ES6

--- script.js
+++ script.js
@@ -1,133 +1,174 @@
 'use strict';
 
+//////////////////////////////////////////////////////////////////////////
+//// Функции-Конструкторы и Оператор new
+
+//const Person = function (firstName, birthYear) {
+//  // Instance properties - свойства экземпляра
+//  this.firstName = firstName;
+//  this.birthYear = birthYear;
+
+//  // Никогда не делать этого
+//  // this.printAge = function() {
+//  //   console.log(2021 - this.birthYear);
+//  // };
+//};
+
+//const youra = new Person('YouRa', 1974);
+//console.log(youra);
+
+//// 1. Создаётся новый пустой объект {}
+//// 2. Вызывается функция, this = {}
+//// 3. {} связывается с прототипом
+//// 4. Функция автоматически возвращает {}
+
+//const mary = new Person('Mary', 2000);
+//const john = new Person('John', 2003);
+//console.log(mary, john);
+
+//const jack = 'Jack';
+
+//console.log(mary instanceof Person);
+//console.log(jack instanceof Person);
+
+//// ////////////////////////////////////////////////////////////////////////
+//// // Прототипы
+
+//console.log(Person.prototype);
+
+//Person.prototype.printAge = function () {
+//  console.log(2021 - this.birthYear);
+//};
+
+//youra.printAge();
+//mary.printAge();
+//john.printAge();
+
+//console.log(youra.__proto__);
+//console.log(youra.__proto__ === Person.prototype);
+
+//console.log(Person.prototype.isPrototypeOf(mary));
+//console.log(Person.prototype.isPrototypeOf(john));
+//console.log(Person.prototype.isPrototypeOf(Person));
+//// .prototypeOfLinkedObjects
+
+//Person.prototype.creatureClass = 'Mammal';
+//console.log(mary, john);
+//console.log(mary.hasOwnProperty('birthYear'));
+//console.log(mary.hasOwnProperty('creatureClass'));
+//console.log(youra.hasOwnProperty('birthYear'));
+//console.log(mary.creatureClass);
+
 ////////////////////////////////////////////////////////////////////////
-// Функции-Конструкторы и Оператор new
+//// Прототипное Наследование для Встроенных Объектов
 
-const Person = function (firstName, birthYear) {
-  // Instance properties - свойства экземпляра
-  this.firstName = firstName;
-  this.birthYear = birthYear;
+//// Person.prototype
+//console.log(youra.__proto__);
 
-  // Никогда не делать этого
-  // this.printAge = function() {
-  //   console.log(2021 - this.birthYear);
-  // };
-};
+//// Object.prototype - верхушка цепи прототипов
+//console.log(youra.__proto__.__proto__);
 
-const youra = new Person('YouRa', 1974);
-console.log(youra);
+//// null
+//console.log(youra.__proto__.__proto__.__proto__);
 
-// 1. Создаётся новый пустой объект {}
-// 2. Вызывается функция, this = {}
-// 3. {} связывается с прототипом
-// 4. Функция автоматически возвращает {}
+//console.log(Person.prototype.constructor);
+//console.dir(Person.prototype.constructor);
 
-const mary = new Person('Mary', 2000);
-const john = new Person('John', 2003);
-console.log(mary, john);
+//const numbers = [3, 4, 2, 5, 8, 8, 8, 2, 2];
+//console.log(numbers.__proto__);
+//console.log(Object.getPrototypeOf(numbers));
+//console.log(numbers.__proto__ === Array.prototype);
+//console.log(numbers.__proto__.__proto__);
 
-const jack = 'Jack';
+//Array.prototype.uniqueElements = function () {
+//  return [...new Set(this)];
+//};
 
-console.log(mary instanceof Person);
-console.log(jack instanceof Person);
+//console.log(numbers.uniqueElements());
 
-// ////////////////////////////////////////////////////////////////////////
-// // Прототипы
+//const h2 = document.querySelector('h2');
+//console.dir(h2);
+//console.dir(n => n * 2);
 
-console.log(Person.prototype);
+////////////////////////////////////////////////////////////////////////
+//// Задание 1
 
-Person.prototype.printAge = function () {
-  console.log(2021 - this.birthYear);
-};
+//// 1. Используйте функцию-конструктор, чтобы реализовать автомобиль - Car. Автомобиль имеет свойства name и speed. Свойство speed - это текущая скорость автомобиля в км / ч.
+//// 2. Реализуйте метод accelerate(), который увеличивает скорость автомобиля на 5 и записывает новую скорость в консоль.
+//// 3. Реализуйте метод break(), который снижает скорость автомобиля на 5 и записывает новую скорость в консоль.
+//// 4. Создайте 2 объекта Car и поэкспериментируйте с вызовами методов accelerate() и break() несколько раз для каждого из них.
 
-youra.printAge();
-mary.printAge();
-john.printAge();
+//// Тестовые данные:
+//// Данные для автомобиля 1: Honda едет со скоростью 120 км / ч
+//// Данные для автомобиля 2: BMW едет со скоростью 150 км / ч.
 
-console.log(youra.__proto__);
-console.log(youra.__proto__ === Person.prototype);
+//const Car = function (name, speed) {
+//  this.name = name;
+//  this.speed = speed;
+//};
 
-console.log(Person.prototype.isPrototypeOf(mary));
-console.log(Person.prototype.isPrototypeOf(john));
-console.log(Person.prototype.isPrototypeOf(Person));
-// .prototypeOfLinkedObjects
+//Car.prototype.accelerate = function () {
+//  this.speed += 5;
+//  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
+//};
 
-Person.prototype.creatureClass = 'Mammal';
-console.log(mary, john);
-console.log(mary.hasOwnProperty('birthYear'));
-console.log(mary.hasOwnProperty('creatureClass'));
-console.log(youra.hasOwnProperty('birthYear'));
-console.log(mary.creatureClass);
+//Car.prototype.breake = function () {
+//  this.speed -= 5;
+//  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
+//};
 
-//////////////////////////////////////////////////////////////////////
-// Прототипное Наследование для Встроенных Объектов
+//const honda = new Car('Honda', 120);
+//const bmw = new Car('BMW', 150);
 
-// Person.prototype
-console.log(youra.__proto__);
+//honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
 
-// Object.prototype - верхушка цепи прототипов
-console.log(youra.__proto__.__proto__);
-
-// null
-console.log(youra.__proto__.__proto__.__proto__);
-
-console.log(Person.prototype.constructor);
-console.dir(Person.prototype.constructor);
-
-const numbers = [3, 4, 2, 5, 8, 8, 8, 2, 2];
-console.log(numbers.__proto__);
-console.log(Object.getPrototypeOf(numbers));
-console.log(numbers.__proto__ === Array.prototype);
-console.log(numbers.__proto__.__proto__);
-
-Array.prototype.uniqueElements = function () {
-  return [...new Set(this)];
-};
-
-console.log(numbers.uniqueElements());
-
-const h2 = document.querySelector('h2');
-console.dir(h2);
-console.dir(n => n * 2);
+//bmw.breake();
+//bmw.breake();
+//bmw.breake();
+//bmw.breake();
 
 //////////////////////////////////////////////////////////////////////
-// Задание 1
+// Классы ES6
 
-// 1. Используйте функцию-конструктор, чтобы реализовать автомобиль - Car. Автомобиль имеет свойства name и speed. Свойство speed - это текущая скорость автомобиля в км / ч.
-// 2. Реализуйте метод accelerate(), который увеличивает скорость автомобиля на 5 и записывает новую скорость в консоль.
-// 3. Реализуйте метод break(), который снижает скорость автомобиля на 5 и записывает новую скорость в консоль.
-// 4. Создайте 2 объекта Car и поэкспериментируйте с вызовами методов accelerate() и break() несколько раз для каждого из них.
+// Class expression
+// const Person = class {
 
-// Тестовые данные:
-// Данные для автомобиля 1: Honda едет со скоростью 120 км / ч
-// Данные для автомобиля 2: BMW едет со скоростью 150 км / ч.
+// }
 
-const Car = function (name, speed) {
-  this.name = name;
-  this.speed = speed;
-};
+// Class declaration
+class Person {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
 
-Car.prototype.accelerate = function () {
-  this.speed += 5;
-  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
-};
+  // Методы добавляются в свойство .prototype
+  printAge() {
+    console.log(2021 - this.birthYear);
+  }
 
-Car.prototype.breake = function () {
-  this.speed -= 5;
-  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
-};
+  greet() {
+    console.log(`Hello! My name is ${this.firstName}!`);
+  }
+}
 
-const honda = new Car('Honda', 120);
-const bmw = new Car('BMW', 150);
+const jack = new Person('Jack', 2000);
+console.log(jack);
+jack.printAge();
+console.log(jack.__proto__ === Person.prototype);
+console.log(Object.getPrototypeOf(jack) === Person.prototype);
 
-honda.accelerate();
-honda.accelerate();
-honda.accelerate();
-honda.accelerate();
-honda.accelerate();
-honda.accelerate();
+// Person.prototype.greet = function () {
+//   console.log(`Hello! My name is ${this.firstName}!`);
+// };
 
-bmw.breake();
-bmw.breake();
-bmw.breake();
-bmw.breake();
+jack.greet();
+
+// 1. Классы не "поднимаются" при помощи hoisting
+// 2. Классы являются "firts class citizens"
+// 3. Тело класса выполняется в strict режиме
