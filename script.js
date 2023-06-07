63ce06dd53502ee449aae08c909377602a6107a6
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-07 12:03:05 +0300

    Task 1

--- script.js
+++ script.js
@@ -89,3 +89,45 @@ console.log(numbers.uniqueElements());
 const h2 = document.querySelector('h2');
 console.dir(h2);
 console.dir(n => n * 2);
+
+//////////////////////////////////////////////////////////////////////
+// Задание 1
+
+// 1. Используйте функцию-конструктор, чтобы реализовать автомобиль - Car. Автомобиль имеет свойства name и speed. Свойство speed - это текущая скорость автомобиля в км / ч.
+// 2. Реализуйте метод accelerate(), который увеличивает скорость автомобиля на 5 и записывает новую скорость в консоль.
+// 3. Реализуйте метод break(), который снижает скорость автомобиля на 5 и записывает новую скорость в консоль.
+// 4. Создайте 2 объекта Car и поэкспериментируйте с вызовами методов accelerate() и break() несколько раз для каждого из них.
+
+// Тестовые данные:
+// Данные для автомобиля 1: Honda едет со скоростью 120 км / ч
+// Данные для автомобиля 2: BMW едет со скоростью 150 км / ч.
+
+const Car = function (name, speed) {
+  this.name = name;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 5;
+  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
+};
+
+Car.prototype.breake = function () {
+  this.speed -= 5;
+  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
+};
+
+const honda = new Car('Honda', 120);
+const bmw = new Car('BMW', 150);
+
+honda.accelerate();
+honda.accelerate();
+honda.accelerate();
+honda.accelerate();
+honda.accelerate();
+honda.accelerate();
+
+bmw.breake();
+bmw.breake();
+bmw.breake();
+bmw.breake();
