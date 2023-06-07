72ae96b38efdb1dcaa4c91cb18c7da8b7230c295
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-07 10:40:33 +0300

    Прототипы

--- script.js
+++ script.js
@@ -30,3 +30,30 @@ const jack = 'Jack';
 
 console.log(mary instanceof Person);
 console.log(jack instanceof Person);
+
+// ////////////////////////////////////////////////////////////////////////
+// // Прототипы
+
+console.log(Person.prototype);
+
+Person.prototype.printAge = function () {
+  console.log(2021 - this.birthYear);
+};
+
+youra.printAge();
+mary.printAge();
+john.printAge();
+
+console.log(youra.__proto__);
+console.log(youra.__proto__ === Person.prototype);
+
+console.log(Person.prototype.isPrototypeOf(mary));
+console.log(Person.prototype.isPrototypeOf(john));
+console.log(Person.prototype.isPrototypeOf(Person));
+// .prototypeOfLinkedObjects
+
+Person.prototype.creatureClass = 'Mammal';
+console.log(mary, john);
+console.log(mary.hasOwnProperty('birthYear'));
+console.log(mary.hasOwnProperty('creatureClass'));
+console.log(youra.hasOwnProperty('birthYear'));
