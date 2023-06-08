e4d76a4bff4c24a8e6d93b00d47acfc84711a66f
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-08 10:54:00 +0300

    Наследование Классов. Функции-конструкторы

--- script.js
+++ script.js
@@ -329,48 +329,93 @@
 // преобразует его в км / ч перед сохранением значения, умножая ввод на 1,6)
 // 4. Создайте новый автомобиль и поэкспериментируйте с методами accelerate() и break(), а также с геттером и сеттером.
 
-class Car {
-  constructor(name, speed) {
-    this.name = name;
-    this.speed = speed;
-  }
+//class Car {
+//  constructor(name, speed) {
+//    this.name = name;
+//    this.speed = speed;
+//  }
 
-  accelerate() {
-    this.speed += 5;
-    console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
-  }
+//  accelerate() {
+//    this.speed += 5;
+//    console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
+//  }
 
-  breake() {
-    this.speed -= 5;
-    console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
-  }
+//  breake() {
+//    this.speed -= 5;
+//    console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
+//  }
 
-  get speedMph() {
-    return this.speed / 1.6;
-  }
+//  get speedMph() {
+//    return this.speed / 1.6;
+//  }
 
-  set speedMph(speed) {
-    this.speed = speed * 1.6;
-  }
-}
+//  set speedMph(speed) {
+//    this.speed = speed * 1.6;
+//  }
+//}
 
-const honda = new Car('Honda', 120);
-const bmw = new Car('BMW', 150);
+//const honda = new Car('Honda', 120);
+//const bmw = new Car('BMW', 150);
 
-honda.accelerate();
-honda.accelerate();
-honda.accelerate();
-honda.accelerate();
-honda.accelerate();
-honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
+//honda.accelerate();
 
-bmw.breake();
-bmw.breake();
-bmw.breake();
-bmw.breake();
+//bmw.breake();
+//bmw.breake();
+//bmw.breake();
+//bmw.breake();
 
-console.log(honda.speedMph);
+//console.log(honda.speedMph);
 
-honda.breake();
-honda.speedMph = 20;
-console.log(honda.speed);
+//honda.breake();
+//honda.speedMph = 20;
+//console.log(honda.speed);
+
+//////////////////////////////////////////////////////////////////////
+
+// Наследование "Классов". Функции-Конструкторы
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+Person.prototype.printAge = function () {
+  console.log(2021 - this.birthYear);
+};
+
+const Student = function (firstName, birthYear, dept) {
+  Person.call(this, firstName, birthYear);
+  this.dept = dept;
+};
+
+// Связываем прототипы
+Student.prototype = Object.create(Person.prototype);
+
+// Student.prototype = Person.prototype;
+
+Student.prototype.introduce = function () {
+  console.log(
+    `My name is ${this.firstName} and I study at the "${this.dept}" department.`
+  );
+};
+
+const jack = new Student('Jack', 2000, 'Programming');
+console.log(jack);
+jack.introduce();
+jack.printAge();
+
+// console.log(jack.__proto__);
+console.log(Object.getPrototypeOf(jack));
+console.log(Object.getPrototypeOf(Object.getPrototypeOf(jack)));
+
+console.log(jack instanceof Student);
+console.log(jack instanceof Person);
+console.log(jack instanceof Object);
+
+Student.prototype.constructor = Student;
+console.dir(Student.prototype.constructor);
