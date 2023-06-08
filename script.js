b9e32039ada63017463b0407af72898e53466142
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-08 12:04:22 +0300

    Наследование классов (extends, super)

--- script.js
+++ script.js
@@ -430,55 +430,120 @@
 // Тестовые данные:
 // Тесла едет со скоростью 100 км / ч, с зарядом 33%
 
-const Car = function (name, speed) {
-  this.name = name;
-  this.speed = speed;
-};
+//const Car = function (name, speed) {
+//  this.name = name;
+//  this.speed = speed;
+//};
 
-Car.prototype.accelerate = function () {
-  this.speed += 5;
-  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
-};
+//Car.prototype.accelerate = function () {
+//  this.speed += 5;
+//  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
+//};
 
-Car.prototype.breake = function () {
-  this.speed -= 5;
-  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
-};
+//Car.prototype.breake = function () {
+//  this.speed -= 5;
+//  console.log(`${this.name} двигается со скоростью ${this.speed} км/ч.`);
+//};
 
-const ElectricCar = function (name, speed, battery) {
-  Car.call(this, name, speed);
-  this.battery = battery;
-};
+//const ElectricCar = function (name, speed, battery) {
+//  Car.call(this, name, speed);
+//  this.battery = battery;
+//};
 
-ElectricCar.prototype = Object.create(Car.prototype);
+//ElectricCar.prototype = Object.create(Car.prototype);
 
-ElectricCar.prototype.chargeBattery = function (chargeLevel) {
-  this.battery = chargeLevel;
-};
+//ElectricCar.prototype.chargeBattery = function (chargeLevel) {
+//  this.battery = chargeLevel;
+//};
 
-ElectricCar.prototype.accelerate = function () {
-  this.speed += 10;
-  this.battery -= 1;
-  console.log(
-    `${this.name} едет со скоростью ${this.speed} км / ч, с зарядом ${this.battery}%`
-  );
-};
+//ElectricCar.prototype.accelerate = function () {
+//  this.speed += 10;
+//  this.battery -= 1;
+//  console.log(
+//    `${this.name} едет со скоростью ${this.speed} км / ч, с зарядом ${this.battery}%`
+//  );
+//};
 
-const tesla = new ElectricCar('Тесла', 100, 33);
-console.log(tesla);
-tesla.chargeBattery(80);
-console.log(tesla);
+//const tesla = new ElectricCar('Тесла', 100, 33);
+//console.log(tesla);
+//tesla.chargeBattery(80);
+//console.log(tesla);
 
-tesla.breake();
-tesla.breake();
-tesla.breake();
+//tesla.breake();
+//tesla.breake();
+//tesla.breake();
 
-tesla.accelerate();
+//tesla.accelerate();
 
-const bmw = new Car('BMW', 150);
+//const bmw = new Car('BMW', 150);
 
-bmw.breake();
-bmw.breake();
+//bmw.breake();
+//bmw.breake();
 
-bmw.accelerate();
-bmw.accelerate();
+//bmw.accelerate();
+//bmw.accelerate();
+
+//////////////////////////////////////////////////////////////////////
+// Наследование "Классов". Классы ES6
+
+class Person {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  // Методы добавляются в свойство .prototype
+  printAge() {
+    console.log(2021 - this.birthYear);
+  }
+
+  greet() {
+    console.log(`Hello! My name is ${this.fullName}!`);
+  }
+
+  get age() {
+    return 2021 - this.birthYear;
+  }
+
+  // Установить значение в уже существующее свойство
+  set fullName(personName) {
+    // console.log(personName);
+    if (personName.includes(' ')) {
+      this._fullName = personName;
+    } else {
+      alert(`Полное имя должно состоять из имени и фамилии!`);
+    }
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static highFive() {
+    console.log('High five!');
+  }
+}
+
+class Student extends Person {
+  constructor(fullName, birthYear, dept) {
+    // Обязательно записать в самом начале!
+    super(fullName, birthYear);
+    this.dept = dept;
+  }
+
+  introduce() {
+    console.log(
+      `My name is ${this.fullName} and I study at the "${this.dept}" department.`
+    );
+  }
+
+  greet() {
+    console.log(`Меня зовут ${this.fullName} и я студент!`);
+  }
+}
+
+const jack = new Student('Jack White', 2001, 'Programming');
+console.log(jack);
+jack.introduce();
+jack.printAge();
+jack.greet();
