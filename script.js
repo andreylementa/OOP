0a6669dcff9cb13edc2aab5f3a01edb15c5127f3
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-08 12:14:37 +0300

    Наследование классов. Object.create()

--- script.js
+++ script.js
@@ -486,64 +486,95 @@
 //////////////////////////////////////////////////////////////////////
 // Наследование "Классов". Классы ES6
 
-class Person {
-  constructor(fullName, birthYear) {
-    this.fullName = fullName;
-    this.birthYear = birthYear;
-  }
+//class Person {
+//  constructor(fullName, birthYear) {
+//    this.fullName = fullName;
+//    this.birthYear = birthYear;
+//  }
 
-  // Методы добавляются в свойство .prototype
+//  // Методы добавляются в свойство .prototype
+//  printAge() {
+//    console.log(2021 - this.birthYear);
+//  }
+
+//  greet() {
+//    console.log(`Hello! My name is ${this.fullName}!`);
+//  }
+
+//  get age() {
+//    return 2021 - this.birthYear;
+//  }
+
+//  // Установить значение в уже существующее свойство
+//  set fullName(personName) {
+//    // console.log(personName);
+//    if (personName.includes(' ')) {
+//      this._fullName = personName;
+//    } else {
+//      alert(`Полное имя должно состоять из имени и фамилии!`);
+//    }
+//  }
+
+//  get fullName() {
+//    return this._fullName;
+//  }
+
+//  static highFive() {
+//    console.log('High five!');
+//  }
+//}
+
+//class Student extends Person {
+//  constructor(fullName, birthYear, dept) {
+//    // Обязательно записать в самом начале!
+//    super(fullName, birthYear);
+//    this.dept = dept;
+//  }
+
+//  introduce() {
+//    console.log(
+//      `My name is ${this.fullName} and I study at the "${this.dept}" department.`
+//    );
+//  }
+
+//  greet() {
+//    console.log(`Меня зовут ${this.fullName} и я студент!`);
+//  }
+//}
+
+//const jack = new Student('Jack White', 2001, 'Programming');
+//console.log(jack);
+//jack.introduce();
+//jack.printAge();
+//jack.greet();
+
+//////////////////////////////////////////////////////////////////////
+// Наследование "Классов". Object.create()
+
+const PersonProto = {
   printAge() {
     console.log(2021 - this.birthYear);
-  }
+  },
 
-  greet() {
-    console.log(`Hello! My name is ${this.fullName}!`);
-  }
+  initPerson(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  },
+};
 
-  get age() {
-    return 2021 - this.birthYear;
-  }
+const StudentProto = Object.create(PersonProto);
+StudentProto.initStudent = function (firstName, birthYear, dept) {
+  PersonProto.initPerson.call(this, firstName, birthYear);
+  this.dept = dept;
+};
 
-  // Установить значение в уже существующее свойство
-  set fullName(personName) {
-    // console.log(personName);
-    if (personName.includes(' ')) {
-      this._fullName = personName;
-    } else {
-      alert(`Полное имя должно состоять из имени и фамилии!`);
-    }
-  }
+StudentProto.introduce = function () {
+  console.log(
+    `My name is ${this.firstName} and I study at the "${this.dept}" department.`
+  );
+};
 
-  get fullName() {
-    return this._fullName;
-  }
-
-  static highFive() {
-    console.log('High five!');
-  }
-}
-
-class Student extends Person {
-  constructor(fullName, birthYear, dept) {
-    // Обязательно записать в самом начале!
-    super(fullName, birthYear);
-    this.dept = dept;
-  }
-
-  introduce() {
-    console.log(
-      `My name is ${this.fullName} and I study at the "${this.dept}" department.`
-    );
-  }
-
-  greet() {
-    console.log(`Меня зовут ${this.fullName} и я студент!`);
-  }
-}
-
-const jack = new Student('Jack White', 2001, 'Programming');
-console.log(jack);
+const jack = Object.create(StudentProto);
+jack.initStudent('Jack', 2003, 'Programming');
 jack.introduce();
 jack.printAge();
-jack.greet();
