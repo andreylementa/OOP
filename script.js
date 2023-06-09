f0cfd3c36eb5540ed01e69c5723f5bb5501a4bc1
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-06-09 09:23:49 +0300

    Methods chaining

--- script.js
+++ script.js
@@ -587,76 +587,83 @@
 // Public methods
 // Private methods
 
-class Account {
-  // Public fields
-  local = navigator.language;
+//class Account {
+//  // Public fields
+//  local = navigator.language;
 
-  // Private fields
-  #transactions = [];
-  #pin;
+//  // Private fields
+//  #transactions = [];
+//  #pin;
 
-  constructor(owner, currency, pin) {
-    this.owner = owner;
-    this.currency = currency;
-    // Protected properties with _
-    this.#pin = pin;
-    // this._transactions = [];
-    // this.local = navigator.language;
+//  constructor(owner, currency, pin) {
+//    this.owner = owner;
+//    this.currency = currency;
+//    // Protected properties with _
+//    this.#pin = pin;
+//    // this._transactions = [];
+//    // this.local = navigator.language;
 
-    console.log(`Спасибо что открыли счёт в нашем банке, ${owner}!`);
-  }
+//    console.log(`Спасибо что открыли счёт в нашем банке, ${owner}!`);
+//  }
 
-  // Public interface (methods) - API
-  deposit(value) {
-    this.#transactions.push(value);
-    return this;
-  }
+//  // Public interface (methods) - API
+//  deposit(value) {
+//    this.#transactions.push(value);
+//    return this;
+//  }
 
-  withdraw(value) {
-    // this.transactions.push(-value);
-    this.deposit(-value);
-    return this;
-  }
+//  withdraw(value) {
+//    // this.transactions.push(-value);
+//    this.deposit(-value);
+//    return this;
+//  }
 
-  setDefaultPin() {
-    this.#pin = '0000';
-  }
+//  setDefaultPin() {
+//    this.#pin = '0000';
+//  }
 
-  getTransactions() {
-    return this.#transactions;
-  }
+//  getTransactions() {
+//    return this.#transactions;
+//  }
 
-  requestLoan(value) {
-    if (this.#approveLoan(value)) {
-      this.deposit(value);
-      console.log(`Займ утверждён!`);
-      return this;
-    }
-  }
+//  requestLoan(value) {
+//    if (this.#approveLoan(value)) {
+//      this.deposit(value);
+//      console.log(`Займ утверждён!`);
+//      return this;
+//    }
+//  }
 
-  static greet() {
-    console.log('Добро пожаловать в "Просто Банк"!');
-  }
+//  static greet() {
+//    console.log('Добро пожаловать в "Просто Банк"!');
+//  }
 
-  // Private methods
-  // Protected methods with _
-  #approveLoan(value) {
-    return true;
-  }
-}
+//  // Private methods
+//  // Protected methods with _
+//  #approveLoan(value) {
+//    return true;
+//  }
+//}
 
-const account1 = new Account('Джек', 'USD', 1111);
+//const account1 = new Account('Джек', 'USD', 1111);
 
-// account1.#transactions.push(500);
-// account1.#transactions.push(-100);
+//// account1.#transactions.push(500);
+//// account1.#transactions.push(-100);
 
-account1.deposit(500);
-account1.withdraw(100);
-account1.requestLoan(10000);
-// account1.#approveLoan(10000);
-account1.setDefaultPin();
+//account1.deposit(500);
+//account1.withdraw(100);
+//account1.requestLoan(10000);
+//// account1.#approveLoan(10000);
+//account1.setDefaultPin();
 
-console.log(account1);
-// console.log(account1.#pin);
-console.log(account1.getTransactions());
-Account.greet();
+//console.log(account1);
+//// console.log(account1.#pin);
+//console.log(account1.getTransactions());
+//Account.greet();
+
+account1
+  .deposit(5000)
+  .deposit(1000)
+  .withdraw(300)
+  .requestLoan(20000)
+  .withdraw(1000);
